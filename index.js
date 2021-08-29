fce079da2681bbf06968e9b993e83a0d6ba448cf
unknown <akosiyawin@gmail.com>
2021-08-29 23:51:48 +0800

    first commit

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-/* eslint-disable import/prefer-default-export */
-export {default as FuiAlexaTheme} from './src/components/alexa/FuiAlexaTheme.vue';
-export {default as FuiAlexaScrollbarTheme} from './src/components/alexa/FuiAlexaScrollbarTheme.vue';
-export {default as FuiAlexaFooter} from './src/components/alexa/FuiAlexaFooter.vue';
-export {default as FuiAlexaContact} from './src/components/alexa/FuiAlexaContact.vue';
-export {default as FuiAlexaTestimonial} from './src/components/alexa/FuiAlexaTestimonial.vue';
-export {default as FuiAlexaProject} from './src/components/alexa/FuiAlexaProject.vue';
-export {default as FuiAlexaPortfolio} from './src/components/alexa/FuiAlexaPortfolio.vue';
-export {default as FuiAlexaServices} from './src/components/alexa/FuiAlexaServices.vue';
-export {default as FuiAlexaQualification} from './src/components/alexa/FuiAlexaQualification.vue';
-export {default as FuiAlexaSkills} from './src/components/alexa/FuiAlexaSkills.vue';
-export {default as FuiAlexaAbout} from './src/components/alexa/FuiAlexaAbout.vue';
-export {default as FuiAlexaHome} from './src/components/alexa/FuiAlexaHome.vue';
-export {default as FuiAlexaNavbar} from './src/components/alexa/FuiAlexaNavbar.vue';
-export {default as FuiAlexaApp} from './src/components/alexa/FuiAlexaApp.vue';
+/* eslint-disable import/prefer-default-import */
+import FuiAlexaTheme from './src/components/alexa/FuiAlexaTheme.vue';
+import  FuiAlexaScrollbarTheme from './src/components/alexa/FuiAlexaScrollbarTheme.vue';
+import  FuiAlexaFooter from './src/components/alexa/FuiAlexaFooter.vue';
+import  FuiAlexaContact from './src/components/alexa/FuiAlexaContact.vue';
+import  FuiAlexaTestimonial from './src/components/alexa/FuiAlexaTestimonial.vue';
+import  FuiAlexaProject from './src/components/alexa/FuiAlexaProject.vue';
+import  FuiAlexaPortfolio from './src/components/alexa/FuiAlexaPortfolio.vue';
+import  FuiAlexaServices from './src/components/alexa/FuiAlexaServices.vue';
+import  FuiAlexaQualification from './src/components/alexa/FuiAlexaQualification.vue';
+import  FuiAlexaSkills from './src/components/alexa/FuiAlexaSkills.vue';
+import  FuiAlexaAbout from './src/components/alexa/FuiAlexaAbout.vue';
+import  FuiAlexaHome from './src/components/alexa/FuiAlexaHome.vue';
+import  FuiAlexaNavbar from './src/components/alexa/FuiAlexaNavbar.vue';
+import  FuiAlexaApp from './src/components/alexa/FuiAlexaApp.vue';
 
-/*
-
-import FuiMambaApp from "./components/mamba/FuiMambaApp";
-import FuiMambaNavbar from "./components/mamba/FuiMambaNavbar";
-import FuiMambaHome from "./components/mamba/FuiMambaHome";
-import FuiMambaSponsor from "./components/mamba/FuiMambaSponsor";
-import FuiMambaSpecs from "./components/mamba/FuiMambaSpecs";
-import FuiMambaCase from "./components/mamba/FuiMambaCase";
-import FuiMambaDiscount from "./components/mamba/FuiMambaDiscount";
-import FuiMambaProducts from "./components/mamba/FuiMambaProducts";
-import FuiMambaFooter from "./components/mamba/FuiMambaFooter";
-import FuiMambaScrollUp from "./components/mamba/FuiMambaScrollUp";
-import FuiMambaScrollReveal from "./components/mamba/FuiMambaScrollReveal";
-import FuiMambaScrollbarTheme from "./components/mamba/FuiMambaScrollReveal";
-import FuiMambaTheme from "./components/mamba/FuiMambaTheme";
-
+import FuiMambaApp from "./src/components/mamba/FuiMambaApp";
+import FuiMambaNavbar from "./src/components/mamba/FuiMambaNavbar";
+import FuiMambaHome from "./src/components/mamba/FuiMambaHome";
+import FuiMambaSponsor from "./src/components/mamba/FuiMambaSponsor";
+import FuiMambaSpecs from "./src/components/mamba/FuiMambaSpecs";
+import FuiMambaCase from "./src/components/mamba/FuiMambaCase";
+import FuiMambaDiscount from "./src/components/mamba/FuiMambaDiscount";
+import FuiMambaProducts from "./src/components/mamba/FuiMambaProducts";
+import FuiMambaFooter from "./src/components/mamba/FuiMambaFooter";
+import FuiMambaScrollUp from "./src/components/mamba/FuiMambaScrollUp";
+import FuiMambaScrollReveal from "./src/components/mamba/FuiMambaScrollReveal";
+import FuiMambaScrollbarTheme from "./src/components/mamba/FuiMambaScrollReveal";
+import FuiMambaTheme from "./src/components/mamba/FuiMambaTheme";
 
 export {
     FuiAlexaFooter,
@@ -60,4 +57,4 @@ export {
     FuiMambaHome,
     FuiMambaNavbar,
     FuiMambaApp
-}*/
+}
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-<!--    <fui-alexa-footer></fui-alexa-footer>-->
     <fui-alexa></fui-alexa>
   </div>
 </template>
 
 <script>
 import FuiAlexa from "./examples/FuiAlexa";
-import {FuiAlexaFooter} from "../index";
 export default {
   name: "App",
   components: {
     FuiAlexa,
-    FuiAlexaFooter
   },
 };
 </script>
